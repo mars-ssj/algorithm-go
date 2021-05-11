ad382c9bee4912eace10de24c6c58bd8fc593f65
shenshaojie <shenshaojie@kuaishou.com>
2021-05-11 09:29:30 +0800

    bubble insertion sort

---
+++ mars_sort/sort.go
@@ -0,0 +1,42 @@
+// Package mars_sort 中实现常见的排序算法
+package mars_sort
+
+// BubbleSort 冒泡排序算法
+func BubbleSort(array []int)  {
+	arrayLen := len(array)
+	if arrayLen <= 1 {
+		return
+	}
+	for i := 0; i < arrayLen; i++ {
+		flag := false
+		for j := 0; j < arrayLen - i - 1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+				flag = true
+			}
+		}
+		if !flag {
+			break
+		}
+	}
+}
+
+// InsertionSort 插入排序算法
+func InsertionSort(array []int)  {
+	arrayLen := len(array)
+	if arrayLen <= 1 {
+		return
+	}
+	for i := 1; i < arrayLen; i++ {
+		value := array[i]
+		j := i - 1
+		for ; j >= 0; j-- {
+			if array[j] > value {
+				array[j+1] = array[j]
+			} else {
+				break
+			}
+		}
+		array[j+1] = value
+	}
+}
---
+++ mars_sort/sort_test.go
@@ -0,0 +1,18 @@
+package mars_sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	array := []int{2, 1, 0, -1, -3}
+	BubbleSort(array)
+	fmt.Println(array)
+}
+
+func TestInsertionSort(t *testing.T) {
+	array := []int{2, 1, 0, -1, -3}
+	InsertionSort(array)
+	fmt.Println(array)
+}
